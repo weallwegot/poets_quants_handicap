b190c98ee937c8a1d638546cdefaa0d4b415343e
tosin <tosin.sonuyi@gmail.com>
2018-05-01 22:46:07 -0400

    empty repo initialized with todo comments for ML

---
+++ machine_learning/main.py
@@ -0,0 +1,4 @@
+"""
+TODO: Parse out the csv from pure text into features that can be used for ML algorithms
+TODO: try out with linear regression for predicting percent likelihood of admission
+"""
