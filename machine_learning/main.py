ecf0d884ba36a83a087e7dfd350f6aa817109591
tosin <tosin.sonuyi@gmail.com>
2019-01-17 00:11:04 +0100

    dumps models from main training script

--- machine_learning/main.py
+++ machine_learning/main.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
 import os
+import pickle
 
 import pandas as pd
 import numpy as np
 
 from data_preprocessing import preprocess_data, preprocess_data_4_catboost
-from models import fit_train_test_cv,gboosting_train_test,catboost_pred,gboosting_pred,linear_regression_pred,linear_ridge_pred,linear_ridge_cv_pred,display_metrics
+from models import fit_train_test_cv, gboosting_train_test, catboost_pred, gboosting_pred, linear_regression_pred, linear_ridge_pred, linear_ridge_cv_pred, display_metrics
 from ML_AP import ApplicantProfile
 from constants import SCHOOLS_REVERSED, TARGET_LABELS
 
@@ -12,15 +14,14 @@ from sklearn.linear_model import LinearRegression, SGDRegressor, ElasticNet, Las
 from sklearn.ensemble import GradientBoostingRegressor
 
 
-
-OUT_DIR = os.path.join(os.path.dirname(__file__),'data_out')
+OUT_DIR = os.path.join(os.path.dirname(__file__), 'data_out')
 
 IN_FILE_NAME = 'pq_data_4_24_18.csv'
 IN_FILE_NAME2 = 'pq_data_10_20_17.csv'
-IN_FILE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)),'data_out',IN_FILE_NAME)
-IN_FILE_PATH2 = os.path.join(os.path.dirname(os.path.dirname(__file__)),'data_out',IN_FILE_NAME2)
+IN_FILE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data_out', IN_FILE_NAME)
+IN_FILE_PATH2 = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data_out', IN_FILE_NAME2)
 
-OUT_FILE_PATH = os.path.join(OUT_DIR,"{}_processed.csv".format(IN_FILE_NAME.replace('.csv','')))
+OUT_FILE_PATH = os.path.join(OUT_DIR, "{}_processed.csv".format(IN_FILE_NAME.replace('.csv', '')))
 input_data_df = pd.read_csv(IN_FILE_PATH)
 other_data_df = pd.read_csv(IN_FILE_PATH2)
 
@@ -40,116 +41,111 @@ combined_df.reset_index(inplace=True)
 # 	display_metrics("Catboost for {}".format(school),predicted_labels,labels)
 
 
-
-school_data_dict,colnames = preprocess_data(data_df=combined_df,output_path=OUT_FILE_PATH)
+school_data_dict, colnames = preprocess_data(data_df=combined_df, output_path=OUT_FILE_PATH)
 print(colnames)
 MODELS = {}
 # would use iteritems, but what if i want to port to python 3.5
-for school,feature_label_d in school_data_dict.items():
+for school, feature_label_d in school_data_dict.items():
 
-	features = feature_label_d['features'].values
-	labels = feature_label_d['labels'].values
+    features = feature_label_d['features'].values
+    labels = feature_label_d['labels'].values
 
-	print "Number of Samples for {}: {}\n".format(school,features.shape[0])
+    print("Number of Samples for {}: {}\n".format(school, features.shape[0]))
 
-	# drop indices from the model
-	features = np.delete(features,0,axis=1)
+    # drop indices from the model
+    features = np.delete(features, 0, axis=1)
 
-	# test model against train data. we are using ALL of the data for training. 
-	# Not splitting for cross validation because the dataset for each school is TINY
-	# predicted_labels_lr = linear_regression_pred(features,labels)
+    # test model against train data. we are using ALL of the data for training.
+    # Not splitting for cross validation because the dataset for each school is TINY
+    # predicted_labels_lr = linear_regression_pred(features,labels)
 
-	# predicted_labels_ridge = linear_ridge_pred(features,labels)
+    # predicted_labels_ridge = linear_ridge_pred(features,labels)
 
-	# predicted_labels_ridge_cv = linear_ridge_cv_pred(features,labels)
+    # predicted_labels_ridge_cv = linear_ridge_cv_pred(features,labels)
 
-	# predicted_labels_gboost = gboosting_pred(features,labels)
+    # predicted_labels_gboost = gboosting_pred(features,labels)
 
+    # colnames[1:] so you dont include index
+    model = fit_train_test_cv(model_obj=None, X_train=features, Y_labels=labels, column_names=colnames[1:])
+    model_str = pickle.dumps(obj=model)
+    with open("models/{}.pickle".format(school.lower()), "wb") as f_h:
+        f_h.write(model_str)
 
-	# colnames[1:] so you dont include index
-	model = fit_train_test_cv(model_obj=None,X_train=features,Y_labels=labels,column_names=colnames[1:])
+    #real_gb,preds_gb = gboosting_train_test(features,labels)
 
+    # display metrics for predicting on the training data for each model
 
-	#real_gb,preds_gb = gboosting_train_test(features,labels)
+    #display_metrics("Gboost Regression for {}".format(school),preds_gb,real_gb)
 
-	# display metrics for predicting on the training data for each model
+    # display_metrics("Linear Regression for {}".format(school),predicted_labels_lr,labels)
 
-	#display_metrics("Gboost Regression for {}".format(school),preds_gb,real_gb)
+    # display_metrics("Ridge for {}".format(school),predicted_labels_ridge,labels)
 
-	# display_metrics("Linear Regression for {}".format(school),predicted_labels_lr,labels)
+    # display_metrics("Ridge with Parameter Tuning for {}".format(school),predicted_labels_ridge_cv,labels)
 
-	# display_metrics("Ridge for {}".format(school),predicted_labels_ridge,labels)
-
-	# display_metrics("Ridge with Parameter Tuning for {}".format(school),predicted_labels_ridge_cv,labels)
-
-	MODELS[school] = model
-
-def find_my_chances(gpa,gmat,age,race,university,major,gender):
-
-	# create list of strings to trigger the applicant profile parsing
-	gpa_str = "{} GPA".format(gpa)
-	gmat_str = "{} GMAT".format(gmat)
-	demo_str = "{a} year old {r} {g}".format(a=age,r=race,g=gender)
-	school_info = "Degree in {m} at {uni} (University)".format(m=major,uni=university)
-
-	app_profile = [gpa_str,gmat_str,demo_str,school_info]
-	odds = ""
-	for school in TARGET_LABELS:
-		odds += "{}: 0.0\n".format(school)
-	ap = ApplicantProfile(app_profile,odds)
+    MODELS[school] = model
 
 
+def find_my_chances(gpa, gmat, age, race, university, major, gender):
 
-	d = {}
-	d["GMAT"] = ap.gmat_score
-	d["GPA"] = ap.gpa
-	d["UNIVERSITY"] = ap.uni
-	d["MAJOR"] = ap.major
-	d["JOBTITLE"] = ap.job_title
-	d["GENDER"] = ap.gender
-	d["RACE"] = ap.race
-	d["AGE"] = ap.age
-	d["INTERNATIONAL"] = ap.international
-	d["ODDS"] = ap.odds.encode('utf-8').strip()
+    # create list of strings to trigger the applicant profile parsing
+    gpa_str = "{} GPA".format(gpa)
+    gmat_str = "{} GMAT".format(gmat)
+    demo_str = "{a} year old {r} {g}".format(a=age, r=race, g=gender)
+    school_info = "Degree in {m} at {uni} (University)".format(m=major, uni=university)
 
-	df = pd.DataFrame(d,index=[0])
-	schooldata_dict,mycolnames = preprocess_data(df)
+    app_profile = [gpa_str, gmat_str, demo_str, school_info]
+    odds = ""
+    for school in TARGET_LABELS:
+        odds += "{}: 0.0\n".format(school)
+    ap = ApplicantProfile(app_profile, odds)
 
+    d = {}
+    d["GMAT"] = ap.gmat_score
+    d["GPA"] = ap.gpa
+    d["UNIVERSITY"] = ap.uni
+    d["MAJOR"] = ap.major
+    d["JOBTITLE"] = ap.job_title
+    d["GENDER"] = ap.gender
+    d["RACE"] = ap.race
+    d["AGE"] = ap.age
+    d["INTERNATIONAL"] = ap.international
+    d["ODDS"] = ap.odds.encode('utf-8').strip()
 
-	print("\n {d}".format(d=d))
-	for school,indf in schooldata_dict.items():
+    df = pd.DataFrame(d, index=[0])
+    schooldata_dict, mycolnames = preprocess_data(df)
 
-		# if missing any columns from training set, add them w/ dummy vals
-		for col in colnames:
-			if col not in indf['features'].columns:
-				indf['features'][col] = 0.0
+    print("\n {d}".format(d=d))
+    for school, indf in schooldata_dict.items():
 
+        # if missing any columns from training set, add them w/ dummy vals
+        for col in colnames:
+            if col not in indf['features'].columns:
+                indf['features'][col] = 0.0
 
-		features_df = indf['features'][colnames]
+        features_df = indf['features'][colnames]
 
-		#print(features_df)
+        # print(features_df)
 
+        df2predictfrom = features_df.values
+        df2predictfrom = np.delete(df2predictfrom, 0, axis=1)
 
-		df2predictfrom = features_df.values
-		df2predictfrom = np.delete(df2predictfrom,0,axis=1)
+        chance = MODELS[school].predict(df2predictfrom)
+        try:
+            pass
+            #print("Coefficients: {}".format(MODELS[school].coef_))
+        except AttributeError as ae:
+            continue
 
-		chance = MODELS[school].predict(df2predictfrom)
-		try:
-			pass
-			#print("Coefficients: {}".format(MODELS[school].coef_))
-		except AttributeError as ae:
-			continue
-
-		if school in ['Harvard','Wharton','Stanford','Booth']:
-			print("{s} odds: {c}".format(s=school,c=chance))
+        if school in ['Harvard', 'Wharton', 'Stanford', 'Booth']:
+            print("{s} odds: {c}".format(s=school, c=chance))
 
 
 find_my_chances(
-	gpa=4.0,
-	gmat=800,
-	age=21,
-	race='black',
-	university='stanford university',
-	major='chemical engineering',
-	gender='male')
-
+    gpa=4.0,
+    gmat=800,
+    age=21,
+    race='black',
+    university='stanford university',
+    major='chemical engineering',
+    gender='male')
