0a601df41092a35fe31120922e7971c56c4b8ebb
tosin <tosin.sonuyi@gmail.com>
2019-01-21 23:57:21 -0500

    use for python3 w/ syntax updates

--- machine_learning/constants.py
+++ machine_learning/constants.py
@@ -1,27 +1,29 @@
+#!/usr/bin/env python3
+
 # schools and their synonyms for label creation
-SCHOOLS = {'Harvard':['Harvard'],
-		   'Stanford':['Stanford'],
-		   'Berkeley':['Berkeley','Haas'],
-		   'Wharton':['Wharton'],
-		   'Tuck':['Tuck','Dartmouth'],
-		   'Kellogg':['Kellogg','Northwestern'],
-		   'Cornell':['Cornell'],
-		   'Duke':['Duke'],
-		   'Booth':['Booth','Chicago'],
-		   'Columbia':['Columbia'],
-		   'Michigan':['Michigan','Ross'],
-		   'NYU':['NYU','New York University'],
-		   'UCLA':['UCLA','Anderson'],
-		   'Sloan':['Sloan','MIT'],
-		   'Yale':['Yale'],
-		   'INSEAD':['INSEAD']
-			}
+SCHOOLS = {'Harvard': ['Harvard'],
+           'Stanford': ['Stanford'],
+           'Berkeley': ['Berkeley', 'Haas'],
+           'Wharton': ['Wharton'],
+           'Tuck': ['Tuck', 'Dartmouth'],
+           'Kellogg': ['Kellogg', 'Northwestern'],
+           'Cornell': ['Cornell'],
+           'Duke': ['Duke'],
+           'Booth': ['Booth', 'Chicago'],
+           'Columbia': ['Columbia'],
+           'Michigan': ['Michigan', 'Ross'],
+           'NYU': ['NYU', 'New York University'],
+           'UCLA': ['UCLA', 'Anderson'],
+           'Sloan': ['Sloan', 'MIT'],
+           'Yale': ['Yale'],
+           'INSEAD': ['INSEAD']
+           }
 
 # create another dictionary with the keys and values reversed
 SCHOOLS_REVERSED = {}
-for k,v in SCHOOLS.iteritems():
-	for name in v:
-		SCHOOLS_REVERSED[name] = k
+for k, v in SCHOOLS.items():
+    for name in v:
+        SCHOOLS_REVERSED[name] = k
 
 TARGET_LABELS = list(set(SCHOOLS_REVERSED.values()))
 
