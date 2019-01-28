f0dbde1cf3cc3e1ac61e9c61bfe16c7cff7611e1
tosin <tosin.sonuyi@gmail.com>
2019-01-27 21:20:39 -0500

    adds visualization for segmented distribution by major

--- machine_learning/visualize.py
+++ machine_learning/visualize.py
@@ -10,14 +10,11 @@ import plotly.graph_objs as go
 from plotly import tools
 
 
-OUT_DIR = os.path.join(os.path.dirname(__file__), 'data_out')
+IN_FILE_NAME = 'pq_data_4_24_18_processed.csv'
+IN_FILE_NAME2 = 'pq_data_10_20_17_processed.csv'
+IN_FILE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'machine_learning', 'data_out', IN_FILE_NAME)
+IN_FILE_PATH2 = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'machine_learning', 'data_out', IN_FILE_NAME2)
 
-IN_FILE_NAME = 'pq_data_4_24_18.csv'
-IN_FILE_NAME2 = 'pq_data_10_20_17.csv'
-IN_FILE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data_out', IN_FILE_NAME)
-IN_FILE_PATH2 = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data_out', IN_FILE_NAME2)
-
-OUT_FILE_PATH = os.path.join(OUT_DIR, "{}_processed.csv".format(IN_FILE_NAME.replace('.csv', '')))
 input_data_df = pd.read_csv(IN_FILE_PATH)
 other_data_df = pd.read_csv(IN_FILE_PATH2)
 
@@ -37,7 +34,7 @@ gpa_layout = go.Layout(title='Training Data GPA Distribution',
                        )
 
 gpa_fig = go.Figure(data=gpa_data, layout=gpa_layout)
-py.plot(gpa_fig)
+# py.plot(gpa_fig)
 
 gmat_data = [go.Histogram(x=combined_df.GMAT.values)]
 gmat_layout = go.Layout(title='Training Data GMAT Score Distribution',
@@ -45,4 +42,24 @@ gmat_layout = go.Layout(title='Training Data GMAT Score Distribution',
                             title='GMAT Scores')
                         )
 gmat_fig = go.Figure(data=gmat_data, layout=gmat_layout)
-py.plot(gmat_fig)
+# py.plot(gmat_fig)
+
+
+minority_df = combined_df[combined_df['STEM_MAJOR'] == 1]
+majority_df = combined_df[combined_df['STEM_MAJOR'] == 0]
+
+
+segment_1 = go.Histogram(
+    x=minority_df.GMAT.values,
+    opacity=0.75
+)
+segment_2 = go.Histogram(
+    x=majority_df.GMAT.values,
+    opacity=0.75
+)
+
+comp_data = [segment_1, segment_2]
+comparison_layout = go.Layout(title='Compared GMAT Distribution',
+                              barmode='overlay')
+comp_fig = go.Figure(data=comp_data, layout=comparison_layout)
+py.plot(comp_fig)
